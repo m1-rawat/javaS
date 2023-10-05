25a164e74a4653755abfa3fb0731f5948a831fee
Mayank Rawat <103522364+m1-rawat@users.noreply.github.com>
2023-10-05 07:16:17 +0000

    06

---
+++ .devcontainer/js/06_arrays.js
@@ -0,0 +1,31 @@
+// Arrays
+
+const myArr=[12,43,54,65,76,66]
+const myHeros=["singhsam","dababngg","delhi_typ"]
+
+console.log(myArr[2]); //print 2nd digit
+console.log(myArr[0]);
+
+const myArr2=new Array(1,2,3,4)
+console.log(myArr2[2]);
+
+
+// Array methods
+
+myArr.push(6)     // array me 6 add kr dega last me krega add 
+myArr.push(7)
+myArr.push(8)
+myArr.pop(8)     // delte krdega 8 array se 
+
+
+myArr.unshift(8) // array ke shuru me krega add
+console.log(myArr);
+
+console.log(myArr.includes(9)) // isme pta chalega ki 9 hai kii niii
+console.log(myArr.indexOf(9))  // 9 ki index poochega 
+
+
+  
+
+
+
