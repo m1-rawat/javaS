74e0524b0b3f5810a17df8d27b6cfbf9729f1fb1
Mayank Rawat <103522364+m1-rawat@users.noreply.github.com>
2023-10-06 07:37:18 +0000

    reduce done

--- .devcontainer/js/12_reduce.js
+++ .devcontainer/js/12_reduce.js
@@ -1,5 +1,33 @@
-const myNumbers=[1,2,3,4,5,6,7,8,9,10]
+// Reduce() method executes a reducer function (that you provide ) on each element of the array, resulting in single output 
+// value
 
-const x=myNumbers.reduce(function (acc,currval){
-    return acc+currval
+// it takes 4 argvument :
+// accumulator, current value , index,array
+
+
+
+// int his question we 
+// sum,product ,avg
+
+// const arr=[18,2,3,4,5,6] 
+
+
+//sum 
+//product
+// let sum= arr.reduce( (accu,currvalue ) => {
+//     // return accu+currvalue
+//     return accu*currvalue
+// })
+// console.log(sum);
+
+//average 
+
+let arr1=[8,2,8,2]
+const avg =arr1.reduce( (accu,currvalue,index,array) =>{
+    const total =accu+currvalue
+    if (index == array.length-1) {
+        return total/array.length;
+    }
+    return total
 })
+
